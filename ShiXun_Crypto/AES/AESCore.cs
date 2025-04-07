b13384d6a90bd5ee1e002fdbcc43909824b59a24
ShiXun0604 <tomcruise890604@gmail.com>
2025-04-07 19:28:46 +0800

    2025/4/7

--- ShiXun_Crypto/AES/AESCore.cs
+++ ShiXun_Crypto/AES/AESCore.cs
@@ -8,10 +8,10 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ShiXun_Crypto.AES
 {
-    public class AESCrypto
+    public class AESCore
     {
         public AESKey AESKey { get; set; }
-        public AESCrypto(string mode = "AES-128")
+        public AESCore(string mode = "AES-128")
         {
             this.AESKey = new AESKey(mode);
         }
@@ -19,34 +19,54 @@ namespace ShiXun_Crypto.AES
         public byte[] encrypt(byte[] data)
         {
             byte[] result = new byte[data.Length];
-            int chunk_count = data.Length/16;            
+            int chunk_count = data.Length/16;
+            
+            byte[] return_data = new byte[data.Length];  // NEW
+            Array.Copy(data, return_data, data.Length);  // NEW
+            int curr_idx = 0;  // NEW
 
             for (int i = 0; i < chunk_count; i++) 
             {
                 // split data into 16 bytes
                 byte[] chunk = new byte[16];
-                Array.Copy(data, i * 16, chunk, 0, 16);
+                Array.Copy(data, i * 16, chunk, 0, 16);                
+            
 
                 // 初始輪
                 chunk = AESCryptoCalcu.AddRoundKey(chunk, this.AESKey.round_key[0]);
+                BasicCalcus.AddRoundKey(ref return_data, ref this.AESKey.round_key[0], curr_idx);  // NEW
                 //Console.WriteLine(BitConverter.ToString(chunk).Replace("-", " "));
+                //Console.WriteLine(BitConverter.ToString(return_data).Replace("-", " "));
 
                 // 中間輪
                 for (int j = 1; j < Statics.ROUNDS[this.AESKey.mode]-1; j++)
                 {
                     chunk = AESCryptoCalcu.SubBytes(chunk);
+                    BasicCalcus.SubBytes(ref return_data, curr_idx);  // NEW                    
                     chunk = AESCryptoCalcu.ShiftRows(chunk);
+                    BasicCalcus.ShiftRows(ref return_data, curr_idx);  // NEW 
                     chunk = AESCryptoCalcu.MixColumns(chunk);
+                    BasicCalcus.MixColumns(ref return_data, curr_idx);  // NEW
                     chunk = AESCryptoCalcu.AddRoundKey(chunk, this.AESKey.round_key[j]);
-                    //Console.WriteLine(BitConverter.ToString(chunk).Replace("-", " "));
+                    BasicCalcus.AddRoundKey(ref return_data, ref this.AESKey.round_key[j], curr_idx);  // NEW
+                    Console.WriteLine(BitConverter.ToString(chunk).Replace("-", " "));
+                    Console.WriteLine(BitConverter.ToString(return_data).Replace("-", " "));
+
                 }
 
                 // 最終輪
                 chunk = AESCryptoCalcu.SubBytes(chunk);
                 chunk = AESCryptoCalcu.ShiftRows(chunk);
                 chunk = AESCryptoCalcu.AddRoundKey(chunk, this.AESKey.round_key[Statics.ROUNDS[this.AESKey.mode]-1]);
-                //Console.WriteLine(BitConverter.ToString(chunk).Replace("-", " "));
+                BasicCalcus.SubBytes(ref return_data, curr_idx);  // NEW
+                BasicCalcus.ShiftRows(ref return_data, curr_idx);  // NEW 
+                BasicCalcus.AddRoundKey(ref return_data, ref this.AESKey.round_key[Statics.ROUNDS[this.AESKey.mode] - 1], curr_idx);  // NEW
+                Console.WriteLine(BitConverter.ToString(chunk).Replace("-", " "));
+                Console.WriteLine(BitConverter.ToString(return_data).Replace("-", " "));
                 Array.Copy(chunk, 0, result, i * 16, 16);
+
+
+                curr_idx += 16;  // NEW
             }
             return result;
         }
---
+++ ShiXun_Crypto/AES/BasicCalcus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiXun_Crypto.AES
+{
+    internal class BasicCalcus
+    {
+        private static byte byte_cache1;
+        private static byte byte_cache2;
+        private static byte byte_cache3;
+        private static byte byte_cache4;
+        internal static void AddRoundKey(ref byte[] return_data, ref byte[] round_key, int curr_idx)
+        {
+            for (int i = curr_idx; i < curr_idx+16; i++)
+            {
+                return_data[i] = (byte)(return_data[i]^round_key[i%16]);
+            }
+        }
+        internal static void SubBytes(ref byte[] return_data, int curr_idx)
+        {
+            for (int i = curr_idx; i < curr_idx+16; i++)
+            {
+                return_data[i] = Statics.SBOX[return_data[i]];
+            }
+        }
+        internal static void ShiftRows(ref byte[] return_data, int curr_idx)
+        {
+            (return_data[curr_idx + 1], return_data[curr_idx+5], return_data[curr_idx + 9], return_data[curr_idx + 13]) = 
+                (return_data[curr_idx + 5], return_data[curr_idx + 9], return_data[curr_idx + 13], return_data[curr_idx + 1]);
+            (return_data[curr_idx + 2], return_data[curr_idx + 6], return_data[curr_idx + 10], return_data[curr_idx + 14]) 
+                = (return_data[curr_idx + 10], return_data[curr_idx + 14], return_data[curr_idx + 2], return_data[curr_idx + 6]);
+            (return_data[curr_idx + 3], return_data[curr_idx + 7], return_data[curr_idx + 11], return_data[curr_idx + 15]) 
+                = (return_data[curr_idx + 15], return_data[curr_idx + 3], return_data[curr_idx + 7], return_data[curr_idx + 11]);
+        }
+        internal static void MixColumns(ref byte[] return_data, int curr_idx)
+        {
+            for (int i = curr_idx; i < curr_idx+16; i += 4)
+            {
+                byte_cache1 = return_data[i];
+                byte_cache2 = return_data[i + 1];
+                byte_cache3 = return_data[i + 2];
+                byte_cache4 = return_data[i + 3];
+
+                return_data[i] = (byte)(Statics.MultiplyTable[0x01, byte_cache1] ^ Statics.MultiplyTable[0x02, byte_cache2] ^ byte_cache3 ^ byte_cache4);
+                return_data[i + 1] = (byte)(byte_cache1 ^ Statics.MultiplyTable[0x01, byte_cache2] ^ Statics.MultiplyTable[0x02, byte_cache3] ^ byte_cache4);
+                return_data[i + 2] = (byte)(byte_cache1 ^ byte_cache2 ^ Statics.MultiplyTable[0x01, byte_cache3] ^ Statics.MultiplyTable[0x02, byte_cache4]);
+                return_data[i + 3] = (byte)(Statics.MultiplyTable[0x02, byte_cache1] ^ byte_cache2 ^ byte_cache3 ^ Statics.MultiplyTable[0x01, byte_cache4]);
+            }
+        }
+    }
+}
